38edcdce84dccd4f31510417e931f64e1578c08e
[Szymon Gorczyca] <[szymon319@interia.eu]>
2021-01-13 00:04:39 +0100

    TypeError

--- overlappingspheres/tests/test_functionality.py
+++ overlappingspheres/tests/test_functionality.py
@@ -1,6 +1,8 @@
+from overlappingspheres.functionality import randompoint_on
+from overlappingspheres.functionality import forces_total
 from shapely.geometry import Point, Polygon
 
-import overlappingspheres
+# import overlappingspheres
 import unittest
 
 
@@ -8,14 +10,14 @@ class TestFunctionality(unittest.TestCase):
 
     def test_randompoint_on(self):
         unitsquare = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
-        y = overlappingspheres.functionality.randompoint_on(unitsquare)
+        y = randompoint_on(unitsquare)
         assert type(y) == Point
         # self.assertEqual(overlappingspheres.greet("Fergus"), "Hello Fergus")
 
     def test_forces_total(self):
         pt = Point([0, 0])
         pts = [Point([1, 1])]
-        y = overlappingspheres.functionality.forces_total(pt, pts)
+        y = forces_total(pt, pts)
         assert len(y) == 2
         # self.assertEqual(overlappingspheres.minimum(1, 2, 3), 1)
         # self.assertEqual(overlappingspheres.minimum(1.2, 2.3), 1.2)
